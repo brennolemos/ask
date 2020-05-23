51fa9dd012da32efe618df0dd3b8e7088e0d1058
Breno Lemos <brenomiros@gmail.com>
2020-05-22 21:38:18 -0300

    Custom CSS

--- client/src/components/Perguntas.jsx
+++ client/src/components/Perguntas.jsx
@@ -2,32 +2,40 @@ import React from "react";
 
 const Perguntas = () => (
   <div className="container">
-    <h3>Realizar Pergunta</h3>
-    <hr/>
-    <form action="">
-      <div className="form-row">
-        <div className="col-md-12">
-          <div className="form-group">
-            <label htmlFor="">Título</label>
-            <input type="text" className="form-control" placeholder="Título" />
-          </div>
-        </div>
-        <div className="col-md-12">
-          <div className="form-group">
-            <label htmlFor="">
-              Descrição
-            </label>
-            <textarea
-              className="form-control"
-              placeholder="Descreva sua pergunta aqui"
-              cols="30"
-              rows="5"
-            ></textarea>
-          </div>
-        </div>
-        <button className="btn btn-success">Perguntar</button>
+    <div className="card">
+      <div className="card-header">
+        <h3>Realizar Pergunta</h3>
       </div>
-    </form>
+
+      <div className="card-body">
+        <form action="">
+          <div className="form-row">
+            <div className="col-md-12">
+              <div className="form-group">
+                <label htmlFor="">Título</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Título"
+                />
+              </div>
+            </div>
+            <div className="col-md-12">
+              <div className="form-group">
+                <label htmlFor="">Descrição</label>
+                <textarea
+                  className="form-control"
+                  placeholder="Descreva sua pergunta aqui"
+                  cols="30"
+                  rows="5"
+                ></textarea>
+              </div>
+            </div>
+            <button className="btn btn-success">Perguntar</button>
+          </div>
+        </form>
+      </div>
+    </div>
   </div>
 );
 
